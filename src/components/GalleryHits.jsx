4299add2342ce85ec8a85e855a29919b1f85e261
mohamed adham <mainmohedadham@gmail>
2023-08-03 02:47:41 +0300

    gallery slice

--- src/components/GalleryHits.jsx
+++ src/components/GalleryHits.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react'
-import ReactPaginate from 'react-paginate'
 import { useFetch } from '../hooks/useFetch'
+import Button from './Button'
 
 // eslint-disable-next-line react/prop-types
-const GalleryHits = ({hasPaginate, hitsPerPage}) => {
-    const [page, setPage] = useState(0)
+const GalleryHits = () => {
     const [galleryHits, setGalleryHits] = useState([])
+    const [hits, setHits] = useState([])
+    const [imgs, setImgs] = useState(5)
     useEffect(() => {
     const apiParams = {
       locationExternalIDs: "5002,6020",
       purpose: 'for-rent',
-      hitsPerPage: hitsPerPage || 25,
-      page: page || 0,
+      hitsPerPage: 25,
+      page: 0,
       lang: 'en',
       sort: "city-level-score",
       rentFrequency: 'monthly',
@@ -22,36 +23,29 @@ const GalleryHits = ({hasPaginate, hitsPerPage}) => {
       setGalleryHits(response.hits)
     }
     GetData();
-  }, [hitsPerPage, page])
-  const handlePaginate = (data) => {
-    setPage(data.selected)
+  }, [])
+
+  useEffect(() => {
+    const slicedHits = galleryHits.slice(0, imgs);
+    setHits(slicedHits)
+  }, [imgs, galleryHits])
+
+  const handleImg = () => {
+    setImgs(prev => prev + 5)
   }
+
   return (
-    <div>
+    <div className='flex flex-col gap-[100px] items-center'>
       <div className="flex gap-10 flex-wrap justify-center">
         {
-          galleryHits.map((hit) => (
+          hits.map((hit) => (
             <div key={hit.id} className='cursor-pointer hover:border-[#4A60A1] p-1 rounded-lg border-[#889099] border-[1px]'>
               <img className=" rounded-md w-[324px] h-[200px]" src={hit.coverPhoto.url} alt="photo" />
             </div>
           ))
         }
       </div>
-      {hasPaginate && 
-        (
-          <ReactPaginate
-            className='flex justify-center gap-5 mt-[80px]'
-            previousLabel={"<"}
-            nextLabel={">"}
-            pageCount={hitsPerPage}
-            onPageChange={handlePaginate}
-            previousClassName="bg-[#EDEFF6] text-[#6D737A] w-[40px] h-[40px] flex items-center justify-center rounded-[4px]"
-            nextClassName="bg-[#4A60A1] text-[#fff] w-[40px] h-[40px] flex items-center justify-center rounded-[4px]"
-            activeClassName="bg-[#4A60A1] text-[#fff] "
-            pageClassName="font-semibold text-[#4A60A1] w-[40px] h-[40px] flex items-center justify-center rounded-[4px]"
-          />
-        )
-      }
+      <Button onClick={handleImg} text='load more!...' color={'text-[#fff]'} bg={'bg-[#4a60a1]'} />
     </div>
   )
 }
