89e137457bc0e3fb58322c0b9fc0a9c76c829904
mohamed adham <mainmohedadham@gmail>
2023-08-04 16:54:55 +0300

    form

--- src/sections/ContactUs.jsx
+++ src/sections/ContactUs.jsx
@@ -1,7 +1,30 @@
 import HeadTitle from '../components/HeadTitle'
 import Button from '../components/Button'
+import { useState } from 'react'
+import { Toaster, toast } from 'react-hot-toast'
 
 const ContactUs = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [comments, setComments] = useState('')
+
+  const handleForm = () => {
+    if(
+      firstName.trim() == '' || 
+      lastName.trim() == '' ||
+      email.trim() == '' ||
+      comments.trim() == ''
+    ) {
+      toast.error("This didn't work, All fields required!")
+    }else {
+      toast.success('Successfully Sent!')
+      setFirstName('')
+      setLastName('')
+      setEmail('')
+      setComments('')
+    }
+  }
   
   return (
     <section id='contact' className='bg-[#EDEFF6] py-[85px] px-3'>
@@ -12,14 +35,15 @@ const ContactUs = () => {
             <h3 className='capitalize font-bold text-[24px] text-[#434343]'>Enquiry Form</h3>
             <p className='text-[#576074] font-normal text-base'>Are you looking for details about a certain property? Ask us a question using the form below.</p>
           </div>
-          <form className='w-full flex flex-col items-center gap-3'>
+          <form onSubmit={(e) => e.preventDefault()} className='w-full flex flex-col items-center gap-3' action='' >
             <div className='flex flex-col sm:flex-row gap-3 items-start justify-center w-full'>
-              <input required id='fname' type="text" placeholder='first name' className='flex flex-col capitalize py-4 px-3 border-[1px] rounded focus:outline-none w-full' />
-              <input required type="text" placeholder='last name' className='flex flex-col capitalize py-4 px-3 border-[1px] rounded focus:outline-none w-full' />
+              <input onChange={(e) => setFirstName(e.target.value)} id='fname' type="text" placeholder='first name' className='flex flex-col capitalize py-4 px-3 border-[1px] rounded focus:outline-none w-full' />
+              <input onChange={(e) => setLastName(e.target.value)} type="text" placeholder='last name' className='flex flex-col capitalize py-4 px-3 border-[1px] rounded focus:outline-none w-full' />
             </div>
-            <input required type="text" placeholder='email id' className=' w-full flex flex-col capitalize py-4 px-3 border-[1px] rounded focus:outline-none flex-1' />
-            <textarea required type="text" placeholder='comments or questions' className='w-full flex flex-col h-40 capitalize py-4 px-3 border-[1px] rounded focus:outline-none flex-1' />
-            <Button text={'Submit'} bg={'bg-[#4A60A1]'} color={'text-[#fff]'} />
+            <input onChange={(e) => setEmail(e.target.value)} type="text" placeholder='email id' className=' w-full flex flex-col capitalize py-4 px-3 border-[1px] rounded focus:outline-none flex-1' />
+            <textarea onChange={(e) => setComments(e.target.value)} type="text" placeholder='comments or questions' className='w-full flex flex-col h-40 capitalize py-4 px-3 border-[1px] rounded focus:outline-none flex-1' />
+            <Button onClick={handleForm} text={'Submit'} bg={'bg-[#4A60A1]'} color={'text-[#fff]'} />
+            <Toaster position='bottom-center' reverseOrder={false} />
           </form>
         </div>
       </div>
